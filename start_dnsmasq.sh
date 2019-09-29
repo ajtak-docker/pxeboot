9555519f2ea3be31c2a5f9698a58ceac9d31d802
Jakub Fridrich <xfridrich@gmail.com>
2019-09-29 14:25:05 +0200

    Disable mount crash

--- start_dnsmasq.sh
+++ start_dnsmasq.sh
@@ -4,14 +4,13 @@ start() {
   /usr/sbin/dnsmasq -k
 }
 
+#mount() {
+#  mkdir -p /etc/tftpboot/images/ubuntu \
+#  && mkdir -p /mnt/nfs \
+#  && mount -t nfs 192.168.0.150:/srv/storage/ /mnt/nfs/ \
+#  && cp /mnt/nfs/casper/vmlinuz /etc/tftpboot/images/ubuntu/vmlinuz \
+#  && cp /mnt/nfs/casper/initrd /etc/tftpboot/images/ubuntu/initrd
+#}
 
-mount() {
-  mkdir -p /etc/tftpboot/images/ubuntu \
-  && mkdir -p /mnt/nfs \
-  && mount -t nfs 192.168.0.150:/srv/storage/ /mnt/nfs/ \
-  && cp /mnt/nfs/casper/vmlinuz /etc/tftpboot/images/ubuntu/vmlinuz \
-  && cp /mnt/nfs/casper/initrd /etc/tftpboot/images/ubuntu/initrd
-}
-
-mount
+#mount
 start

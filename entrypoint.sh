0cd0823fe36ca27078c6238538b57574cbb08ad9
Jakub Fridrich <xfridrich@gmail.com>
2019-09-29 08:49:44 +0200

    Comment mounting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## NFS Mount -- outside dockerhub
-mkdir /mnt/nfs \
-    && mount -t nfs ${NFS_PATH} /mnt/nfs/ \
-    && cp /mnt/nfs/casper/vmlinuz /etc/tftpboot/images/ubuntu/vmlinuz \
-    && cp /mnt/nfs/casper/initrd /etc/tftpboot/images/ubuntu/initrd \
-    && umount /mnt/nfs \
-    && rm -rf /mnt/nfs \
+#mkdir /mnt/nfs \
+#    && mount -t nfs ${NFS_PATH} /mnt/nfs/ \
+#    && cp /mnt/nfs/casper/vmlinuz /etc/tftpboot/images/ubuntu/vmlinuz \
+#   && cp /mnt/nfs/casper/initrd /etc/tftpboot/images/ubuntu/initrd \
+#   && umount /mnt/nfs \
+#   && rm -rf /mnt/nfs \
 
 systemctl start dnsmasq \
     && systemctl start tftpd \
